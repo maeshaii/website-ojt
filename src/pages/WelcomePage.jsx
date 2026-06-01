4f263e7e81621339b2ade94ab9945cda389a02a9
maeshaii <kensakitachiiiii@gmail.com>
2026-06-01 23:06:21 +0800

    k

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from './Landingpage/hero';
 import AboutUs from './Landingpage/aboutus';
-import Services from './Landingpage/Services';
+import Services from './Landingpage/services';
 import Footer from '../components/Footer';
 
 export default function WelcomePage() {
